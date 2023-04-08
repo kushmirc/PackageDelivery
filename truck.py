fbc3efd71086caff2452c8d885ab9ad3bb52a8c8
kushmirc <107213928+kushmirc@users.noreply.github.com>
2023-04-07 20:50:35 -0600

    Add comments to truck.py

--- truck.py
+++ truck.py
@@ -1,25 +1,14 @@
+#A module to create and load trucks, and run the core of the program.
 import sys
-
 import package
 import datetime
 
-
+#Define time object and initialize it to a zero value.
 time_obj = 0.0
+#Define a varible to hold the time value input by the user and initialize it to a value always greater than the time_obj.
 time_input_hours = 24.0
 
-'''time_input_dt = datetime.strptime(time_input, '%H:%M')
-print(time_input_dt)
-time_input_hours = datetime.timestamp(time_input_dt)
-
-run_program = input('Would you like to run the package delivery program?' '\n' '  press \'y\' for yes' '\n'   '  press \'q\' for quit' '\n')
-if run_program == 'q':
-    sys.exit()
-while run_program != 'y':
-    if run_program == 'q':
-        sys.exit()
-    run_program = input('invalid entry, please try again' '\n')
-    #continue'''
-
+#Display a starting menu in the command prompt
 print('Please type an option number and press \'enter\' and option:')
 print('-------------------------')
 print('1. Print all package status and total mileage')
@@ -27,61 +16,65 @@ print('2. Get a single package status with a time')
 print('3. Get all package status with a time')
 print('4. Exit the program' '\n')
 
+#capture the selection made by the user.
 user_input = input()
 
+#if the user enters an invalid option, display an error message and wait for another input.
+#if the user quits by entering 4 at any point, quit the program.
+#if the user enters 1, run the program.
+while user_input != '1' and user_input != '2' and user_input != '3':
+    if user_input == '4':
+        sys.exit()
+    user_input = input('Invalid entry, please try again' '\n')
+
+#if the user enters 2, capture a package number and time value to look up.
 if user_input == '2':
     package_input = int(input('Please enter a package number:' '\n'))
     while package_input < int('1') or int(package_input) > int('40'):
         package_input = int(input('Invalid package number, please try again' '\n'))
     time_input = input('Please enter a time (ex. \'10:15\')' '\n')
+    #convert the time string from the user to a float value for hours by first splitting at the semicolon.
+    #then convert each split portion to a float, divide the minutes by 60, and add them together.
     time_input_split = time_input.split(":")
     time_input_hours = float(time_input_split[0]) + (float(time_input_split[1]) / 60)
 
+#if the user enters 3, capture a time value to look up.
 if user_input == '3':
     time_input = input('Please enter a time (ex. \'10:15\')' '\n')
+    #this is the same string-to-float time conversion described above.
     time_input_split = time_input.split(":")
     time_input_hours = float(time_input_split[0]) + (float(time_input_split[1]) / 60)
 
-if user_input == '4':
-    sys.exit()
-while user_input != '1' and user_input != '2' and user_input != '3':
-    if user_input == '4':
-        sys.exit()
-    user_input = input('Invalid entry, please try again' '\n')
-    #continue
-
-
-
-
-
+#Define the Truck class with a constructor to instantiate Truck objects.
 class Truck:
-
     def __init__(self):
-        #self.loaded_packages = []
         self.name = ''
         self.loaded_packages = []
         self.time = 0.0
         self.miles = 0.0
 
-def  timeToDeliver(distance):  #time measured in hours
-    time_to_deliver = distance / 18
-    #print(time_to_deliver)
+#Calculate the time taken to deliver a package (as a float in hours) with a given distance assuming trucks always drive at 18mph.
+def  timeToDeliver(distance):
+    time_to_deliver = distance / 18  #time measured in hours
     return time_to_deliver
 
 
-#timeToDeliver(9)
-
+#Instantiate Truck1, set its name, and set its departure time from the hub.
 truck1 = Truck()
 truck1.name = 'Truck 1'
 truck1.time = 8.0
-#print (str(datetime.timedelta(hours= truck1.time))) # [:-3])
+
+#Instantiate Truck2, set its name, and set its departure time from the hub.
 truck2 = Truck()
 truck2.name = 'Truck 2'
 truck2.time = 9.0833333333334
+
+#Instantiate Truck3, set its name, and set its departure time from the hub.
 truck3 = Truck()
 truck3.name = 'Truck 3'
 truck3.time = 10.33333333334
 
+#define a method to load the packages that will go on Truck 1.
 def loadTruck1():
     import main
     truck1.loaded_packages.append(package.package1.package_id)
@@ -110,8 +103,8 @@ def loadTruck1():
     main.packageHash.update_delivery(37, 'en route', '')
     truck1.loaded_packages.append(package.package40.package_id)
     main.packageHash.update_delivery(40, 'en route', '')
-#print('Truck1 Packages: ', truck1.loaded_packages)
 
+#define a method to load the packages that will go on Truck 2.
 def loadTruck2():
     import main
     truck2.loaded_packages.append(package.package3.package_id)
@@ -136,12 +129,15 @@ def loadTruck2():
     main.packageHash.update_delivery(38, 'en route', '')
     truck2.loaded_packages.append(package.package39.package_id)
     main.packageHash.update_delivery(39, 'en route', '')
-#print('Truck2 Packages: ', truck2.loaded_packages)
 
+#define a method to load the packages that will go on Truck 3.
+#since a package from Truck 3 will have a special message, display the message when the full program is run
+#(the full program runs when there is no time input by the user, a.k.a., option 1).
 def loadTruck3():
     import main
     if time_input_hours == 24.0:
         print('We were informed at 10:20am that the address originally provided for package number 9 is incorrect.  The correct address is 410 S State St., Salt Lake City, UT 84111. The address is being corrected now.')
+    #update package 9 per the special instructions.
     main.packageHash.insert(package.package9.package_id, '410 S State St', package.package9.city, package.package9.state, '84111', package.package9.delivery_deadline, package.package9.weight, package.package9.special_notes, package.package9.delivery_status)
     truck3.loaded_packages.append(package.package2.package_id)
     main.packageHash.update_delivery(2, 'en route', '')
@@ -175,80 +171,83 @@ def loadTruck3():
     main.packageHash.update_delivery(26, 'en route', '')
     truck3.loaded_packages.append(package.package27.package_id)
     main.packageHash.update_delivery(27, 'en route', '')
-#print('Truck3 Packages: ', truck3.loaded_packages)
 
+#This is the core algorithm which executes the "nearest neighbor" algorithm accomplished by looping the minDistanceFrom function.
 def truckDeliverPackages(truck_number):
     import distance
     import main
     global time_obj
-    #delivered_packages = set()
+    #The first address is always the hub, so the minDistanceFrom is run once at the beginning from the hub.
     min_distance, closest_address, closest_pkg = distance.minDistanceFrom('4001 South 700 East', truck_number.loaded_packages)
+    #add the miles to the first nearest address to the miles traveled by the truck.
     truck_number.miles = truck_number.miles + min_distance
+    #add the time to the first nearest address to the time traveled by the truck.
     truck_number.time = truck_number.time + timeToDeliver(min_distance)
+    #update the global time object to reflect the latest time for the truck.
     time_obj = truck_number.time
+    #check to see if the time input by the user is prior to the truck's time after delivering the package.
+    #if so, exit the function because no more deliveries should be made at this point.
     if time_obj > time_input_hours:
             return
-    #truck_number.miles = float(decimal.Decimal(truck_number.miles)) + float(decimal.Decimal.min_distance)
-    '''print('Closest address,', closest_address, ', is', min_distance, "miles away. Package # is:", closest_pkg, '. Truck miles is:', truck_number.miles, ". Truck time is:", truck_number.time, '.', 'time_obj is:', time_obj)'''
+    #if the user hasn't entered a time (they chose option 1), then log the delivery for the full report.
     if time_input_hours == 24.0:
         print(truck_number.name, 'has delivered package #', closest_pkg, 'to', closest_address, 'at', str(datetime.timedelta(hours=truck_number.time)))
-    #print(closest_address)
-    #print(closest_pkg)
+    #now that the package has been delivered, remove it from the truck.
     truck_number.loaded_packages.remove(closest_pkg)
+    #update the delivery status of the delivered package in the hash table.
     main.packageHash.update_delivery(closest_pkg, 'delivered', str(datetime.timedelta(hours= truck_number.time)))
-    #print('Package', closest_pkg, 'delivered!')
-    #delivered_packages.add(closest_pkg)
+    #for the remainder of the packages in the truck, look through the minDistanceFrom function (nearest neighbor)
+    #to deliver to the next nearest package address at each iteration, using the previous address as the From Address.
+    #the rest of the loop works the same as the steps above.
     for i in range(len(truck_number.loaded_packages)):
-        #if pkg not in delivered_packages:
-        #print('Package not delivered yet')
         min_distance, closest_address, closest_pkg = distance.minDistanceFrom(closest_address, truck_number.loaded_packages)
         truck_number.miles = truck_number.miles + min_distance
         truck_number.time = truck_number.time + timeToDeliver(min_distance)
         time_obj = truck_number.time
         if time_obj > time_input_hours:
             return
-        '''print('Closest address,', closest_address, ', is', min_distance, "miles away. Package # is:", closest_pkg, '. Truck miles is:', truck_number.miles, ". Truck time is:", truck_number.time, '.', 'time_obj is:', time_obj)'''
         if time_input_hours == 24.0:
             print(truck_number.name, 'has delivered package #', closest_pkg, 'to', closest_address, 'at', str(datetime.timedelta(hours=truck_number.time)))
-        #print(min_distance)
-        #print(closest_address)
-        #print(closest_pkg)
-        #delivered_packages.add(closest_pkg)
         truck_number.loaded_packages.remove(closest_pkg)
         main.packageHash.update_delivery(closest_pkg, 'delivered', str(datetime.timedelta(hours=truck_number.time)))
-        #print('Package', closest_pkg, 'delivered!')
+    #once loaded_packages list is empty (all packages delivered) the look will end, and the truck will travel back to the hub.
+    #add the miles and the time for the trip back to the hub, and update the global time object.
     truck_number.miles = truck_number.miles + distance.distanceBetween(closest_address, '4001 South 700 East')
     truck_number.time = truck_number.time + timeToDeliver(min_distance)
     time_obj = truck_number.time
 
+#load truck 1 and deliver its packages if the time input is either the default value, or if it's after the departure time of the truck.
 if time_input_hours > truck1.time:
     loadTruck1()
     truckDeliverPackages(truck1)
-#print(str(datetime.timedelta(hours=time_obj)))
-#print()
+
+#load truck 2 and deliver its packages if the time input is either the default value, or if it's after the departure time of the truck.
 if time_input_hours > truck2.time:
     loadTruck2()
     truckDeliverPackages(truck2)
-#print(str(datetime.timedelta(hours=time_obj)))
-#print()
 
+#load truck 3 and deliver its packages if the time input is either the default value, or if it's after the departure time of the truck.
 if time_input_hours > truck3.time:
     loadTruck3()
     truckDeliverPackages(truck3)
 print()
 
+#run these statements if the user chose option 2 or 3 (time_input_hours is not the default value):
 if time_input_hours != 24.0:
     import main
+    #for option 3, print the information for all of the packages in the hash table.
     if user_input == '3':
         for bucket in main.packageHash.table:
             for pkg in bucket:
                 print(pkg)
+    #for option 2, print the information for just the package number input by the user.
     elif user_input == '2':
         for bucket in main.packageHash.table:
             for pkg in bucket:
                 if pkg[0] == package_input:
                     print(pkg)
 
+#print truck reports and total mileage if the user chose option 1 (time_input_hours remains as the default value):
 if time_input_hours == 24.0:
     print('Truck 1 Report:')
     print('------------------------')
@@ -275,4 +274,3 @@ if time_input_hours == 24.0:
     print()
 
     print('Total miles driven by all trucks: %s' % (truck1.miles + truck2.miles + truck3.miles))
-    #print('Total drive time to deliver all packages: ')
